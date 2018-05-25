6378d38c299ae2f883d240a41001baea7c5df210
Margaret Chu <31119028+magcurly@users.noreply.github.com>
2018-05-25 11:11:29 +0800

    Delete acc2TaxaSQL.R

--- nt_blast_anno/acc2TaxaSQL.R
+++
@@ -1,9 +0,0 @@
-#Build an ass2tax SQL database before performing the annotation
-library("taxonomizr")
-read.accession2taxid(list.files('/Path/to/','accession2taxid.gz$'),'/path/to/your/accessionTaxa.sql')
-
-#The directory must contain 4 files to build the sql database as following: 
-#nucl_est.accession2taxid.gz
-#nucl_gb.accession2taxid.gz
-#nucl_gss.accession2taxid.gz
-#nucl_wgs.accession2taxid.gz
